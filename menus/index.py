4d1acb740b440396b6e94eb58bf96074fb297b56
ANTONIIOSC7\anton <antonio.saborido01@gmail.com>
2024-01-11 09:45:47 +0100

    Eliminación de mapas fuera de la carpeta mapas

--- menus/index.py
+++ menus/index.py
@@ -7,19 +7,20 @@ import menus.gameOver as gameOver
 import menus.character as character
 
 def index(screen, game, menu):
-    if(menu == 1):
-        return main.main_menu(screen, game)  # Llamar a la función main dentro del módulo menus.main
-    elif(menu == 2):
-        return restart.restart_menu(screen, game)
-    elif(menu == 3):
-        return gameOver.game_over_menu(screen, game)
-    elif(menu == 4):
-        return win.win_menu(screen, game)
-    elif(menu == 5):
-        return level.level_selection_menu(screen, game)
-    elif(menu == 6):
-        return character.character_selection_menu(screen, game)
-    elif(menu == 7):
-        return config.config_menu(screen, game)
-    else:
-        return None
+    while True:
+        if(menu == 1):
+            return main.main_menu(screen, game)  # Llamar a la función main dentro del módulo menus.main
+        elif(menu == 2):
+            return restart.restart_menu(screen, game)
+        elif(menu == 3):
+            return gameOver.game_over_menu(screen, game)
+        elif(menu == 4):
+            return win.win_menu(screen, game)
+        elif(menu == 5):
+            return level.level_selection_menu(screen, game)
+        elif(menu == 6):
+            return character.character_selection_menu(screen, game)
+        elif(menu == 7):
+            return config.config_menu(screen, game)
+        else:
+            return None
